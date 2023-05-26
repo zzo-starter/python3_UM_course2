108ab450a0b11ba5a778255fa5796456a674ed1a
Ross Leon <132277790+ross-leon@users.noreply.github.com>
2023-05-25 20:49:25 -0700

    final

--- loops/B-for_loops.py
+++ loops/B-for_loops.py
@@ -1,45 +1,48 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    A-while_loops.py                                   :+:      :+:    :+:    #
+#    B-for_loops.py                                     :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: zoozdev777 <zoozdev777@gmail.com>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/05/24 01:14:52 by zoozdev777        #+#    #+#              #
-#    Updated: 2023/05/25 17:25:45 by zoozdev777       ###   ########.fr        #
+#    Updated: 2023/05/25 17:25:55 by zoozdev777       ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 def header(s):
     print('\n=========', s.upper(), '============')
 
-
+ 
 #====================
-header('WHILE LOOP')
+header('FOR LOOP')
 def sumTo(aBound):
     theSum = 0
-    aNumber = 1
-    while aNumber <= aBound: #1234
-        theSum += aNumber
-        aNumber +=1
+    for i in range(aBound+1): #0123 +1 step 1
+        theSum += i
     return theSum 
 print(sumTo(4))
 
+#====================
+header('FOR LOOP')
+def sumTo(aBound):
+    theSum = 0
+    for i in range(1, aBound+1): #123 +1 step 1
+        theSum += i
+    return theSum 
+print(sumTo(4))
 
 #====================
-header('WHILE LOOP')
-count=0
-even_nums = [] 
-while count <= 15:
-    if count % 2 == 0:
-        even_nums.append(count)
-    count +=1
-print(even_nums)
+header('FOR LOOP')
+def sumTo(aBound):
+    theSum = 0
+    for i in range(1, aBound+1, 2): #123 +1; step 2
+        theSum += i
+    return theSum 
+print(sumTo(4))
 
-
- 
 #====================
-header('FOR LOOP AS WHILE LOOP')
+header('FOR LOOP')
 list1 = [8,3,4,5,6,7,9]
 tot =0
 for n in list1:
--- loops/README.md
+++ loops/README.md
@@ -1,7 +1,9 @@
 # LOOPS
 
-- A while
-- B listener
-- C break and continue
-- D infinite loops
+- A while loops
+- B for loops
+- C listener loops (~input)
+- D break and continue (while and for)
+- E infinite loops (bug)
+
  
